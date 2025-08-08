b02da3afa62596d288f5186a664217e43bb1e873
EdTedesco <eft@psi.edu>
2025-08-08 13:31:36 -0600

    Delete Rename_Date_Files.ps1

--- Rename_Date_Files.ps1
+++
@@ -1,8 +0,0 @@
-# Renames YYMMDD_nnn.fits to YYMMDDa_nnn.fits
-Get-ChildItem -Filter '*.fits' | ForEach-Object {
-    
-#   $newName = $_.BaseName + $suffix + $_.Extension
-    $newName = ($_.name -replace '_','a_')	
-    Rename-Item -path $_ -NewName $newName
-}
-exit
